972d6c4fd0ae8f2453e92ad2e139fd46f8ac59a7
ecc97 <carmonaedwin1123@gmail.com>
2024-10-07 15:18:17 -0400

    feat: get product for id

--- src/app/api/routes/products/[id]/route.ts
+++ src/app/api/routes/products/[id]/route.ts
@@ -1,5 +1,36 @@
 import { NextRequest, NextResponse } from "next/server";
+import { urlApi } from "@/app/api/ApiUrl";
+import { getToken } from "next-auth/jwt";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    
+    try {
+        const { id } = params;
+        
+        const token = await getToken({ req: request });
+        
+        if (!token) {
+            return NextResponse.json({ message: "No autorizado" }, { status: 401 });
+        }
+
+        const response = await fetch(`${urlApi}/auth/products/${id}`, {
+            headers: {
+                'Authorization': `Bearer ${token.accessToken}`,
+                'Content-Type': 'application/json',
+            },
+        });
+
+        if (!response.ok) {
+            if (response.status === 404) {
+                return NextResponse.json({ message: "Producto no encontrado" }, { status: 404 });
+            }
+            throw new Error(`error: ${response.status}`);
+        }
+
+        const data = await response.json();
+        
+        return NextResponse.json(data);
+    } catch (error) {
+        console.error("No se puede mostrar el producto:", error);
+        return NextResponse.json({ message: "No se puede mostrar el producto." }, { status: 500 });
+    }
 }
--- src/app/details/[id]/page.tsx
+++ src/app/details/[id]/page.tsx
@@ -1,48 +1,107 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
+import { useSession, signOut } from "next-auth/react";
 import axios from "axios";
 import { Product } from "@/interface/IProducts";
+import { useRouter } from "next/navigation";
+import Navbar from "@/components/Navbar/Navbar";
+import Button from "@/components/ui/Button.ui";
+import SelectLanguage from "@/components/SelectLanguage/SelectLanguage";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
 
 const ProductDetails = ({ params }: { params: { id: string } }) => {
-    const [product, setProduct] = useState<Product | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  const t = useTranslations("ProductPageView");
 
-    useEffect(() => {
-        const getProduct = async (id: string) => {
-            try {
-                const response = await axios.get(`/api/routes/products/${id}`);
-                const data = await response.data; 
-                setProduct(data);
+  const handleSignOut = async () => {
+    await signOut({ redirect: false });
+    router.push("/login");
+  };
 
-            } catch (error) {
-                console.error("Error al obtener el producto:", error);
-                return null; 
-            } finally {
-                setLoading(false);
-            }
-        };
+  useEffect(() => {
+    const getProduct = async (id: string) => {
+      if (status === "unauthenticated") {
+        router.push("/login");
+        return;
+      }
 
-        getProduct(params.id);
-    }, [params.id]);
+      if (status !== "authenticated") {
+        return;
+      }
 
-    if (loading) {
-        return <p>Cargando...</p>; // Muestra un mensaje mientras se cargan los datos
+      try {
+        const response = await axios.get(`/api/routes/products/${id}`, {
+          headers: {
+            Authorization: `Bearer ${session?.accessToken}`,
+          },
+        });
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error al obtener el producto:", error);
+        if (axios.isAxiosError(error) && error.response?.status === 401) {
+          setError("No autorizado. Por favor, inicie sesión nuevamente.");
+          router.push("/login");
+        } else {
+          setError(
+            "Error al obtener el producto. Por favor, intente nuevamente."
+          );
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    if (status === "authenticated") {
+      getProduct(params.id);
     }
+  }, [params.id, session, status, router]);
 
-    if (!product) {
-        return <p>No se encontró el producto.</p>; // Muestra este mensaje si no se encontró el producto
-    }
-    
+  if (loading) {
+    return <p>Cargando...</p>;
+  }
 
-    return (
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!product) {
+    return <p>No se encontró el producto.</p>;
+  }
+
+  return (
+    <div>
+      <Navbar>
+        <h1>{t("title")}</h1>
         <div>
-            <h1>{product.title}</h1>
-            <img src={product.image} alt={product!.title} />
-            <p>{product.description}</p>
-            <p><strong>Category:</strong> {product.category}</p>
-            <p><strong>Price:</strong> ${product.price}</p>
+          {status === "authenticated" ? (
+            <Button type="button" onClick={handleSignOut}>
+              {t("logoutButton")}
+            </Button>
+          ) : (
+            <Button type="button" onClick={() => router.push("/login")}>
+              {t("loginButton")}
+            </Button>
+          )}
+          <Link href='/products'>{t("productsTitle")}</Link>
+          <SelectLanguage />
         </div>
-    );
+      </Navbar>
+      <h1>{product.title}</h1>
+      <img src={product.image} alt={product!.title} />
+      <p>{product.description}</p>
+      <p>
+        <strong>Category:</strong> {product.category}
+      </p>
+      <p>
+        <strong>Price:</strong> ${product.price}
+      </p>
+    </div>
+  );
 };
 
 export default ProductDetails;
